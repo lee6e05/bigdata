01bba32a3415e1b9d6bcb8d212f07a4e86005c8b
lee6e05 <shuqstar@hotmail.com>
2017-02-28 09:10:13 +0800

    test

--- js/app.js
+++ js/app.js
@@ -1,5 +1,14 @@
-  var app = angular.module("app", []);
-  app.controller("appCtrl", function ($scope) {
-      $scope.names = [1, 2, 3, 4, 5];
+ "use strict";
+ var app = angular.module("app", []);
 
-  });
+ app.controller("appCtrl", function ($scope) {
+
+   $scope.names = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+ });
+
+ app.filter('parseIntFilter', function () {
+   return function (item) {
+     return Math.floor(item / 2);
+   }
+ })
